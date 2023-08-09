312f2f2edef13d6326ca70d1e6d78fea9e113804
Jenipher Oduor <jenipheroduor@gmail.com>
2023-08-09 19:58:41 +0300

    Commit

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const doctorName = document.getElementById("doctor");
+const doctorList = document.getElementById("doctor-list")
 const appointmentButton = document.querySelector('.button');
 const appointmentModal = document.querySelector('.modal');
 const closeModalButton = document.querySelector('.close');
 
+function fetchData(doctorID)
+{
+  fetch(`http://localhost:3000/data/${doctorID}`)
+  .then(response => response.json())
+  .then(data => doctorName.textContent = data.first_name)
+}
+function fetchDoctors()
+   {
+    fetch('http://localhost:3000/data/')
+     .then(response => response.json())
+     .then(data => {
+      if (doctor.first_name)
+      {
+        const li = document.createElement('li')
+        li.innerText = doctor.first_name;
+        li.addEventListener("click", () => {
+          fetchData(doctor.id);
+        })
+        doctorList.appendChild(li)
+      }
+    })
+      
+ }
+   
 appointmentButton.addEventListener('click', () => {
   appointmentModal.style.display = 'block';
 });
